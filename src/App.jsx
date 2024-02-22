4af74ecc710b8a635cca7eb202fa2da19957a4dc
Matos52 <Matej.Regec@gmail.com>
2024-02-22 22:18:20 +0100

    Second commit, added new logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,49 @@
 import Header from "./components/Header";
 import UserInput from "./components/UserInput";
+import Result from "./components/Result";
+import { useState } from "react";
 
 function App() {
+  const [initialInvestment, setInitialInvestment] = useState("1");
+  const [annualInvestment, setAnnualInvestment] = useState("1");
+  const [expectedReturn, setExpectedReturn] = useState("1");
+  const [duration, setDuration] = useState("1");
+
+  function handleInitialInvestment(event) {
+    setInitialInvestment(event.target.value);
+  }
+
+  function handleAnnualInvestment(event) {
+    setAnnualInvestment(event.target.value);
+  }
+
+  function handleExpectedReturn(event) {
+    setExpectedReturn(event.target.value);
+  }
+
+  function handleDuration(event) {
+    setDuration(event.target.value);
+  }
+
   return (
     <div>
       <Header />
-      <UserInput />
+      <UserInput
+        onInitialInvestment={handleInitialInvestment}
+        initialInvestmentValue={initialInvestment}
+        onAnnualInvestment={handleAnnualInvestment}
+        annualInvestmentValue={annualInvestment}
+        onExpectedReturn={handleExpectedReturn}
+        expectedReturnValue={expectedReturn}
+        onDuration={handleDuration}
+        durationValue={duration}
+      />
+      <Result
+        initialInvestmentValue={initialInvestment}
+        annualInvestmentValue={annualInvestment}
+        expectedReturnValue={expectedReturn}
+        durationValue={duration}
+      />
     </div>
   );
 }
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,24 +1,53 @@
-export default function UserInput() {
+export default function UserInput({
+  onInitialInvestment,
+  initialInvestmentValue,
+  onAnnualInvestment,
+  annualInvestmentValue,
+  onExpectedReturn,
+  expectedReturnValue,
+  onDuration,
+  durationValue,
+}) {
   return (
     <section id="user-input">
       <div className="input-group">
         <p>
           <label>INITIAL INVESTMENT</label>
-          <input type="number" required></input>
+          <input
+            type="number"
+            required
+            value={initialInvestmentValue}
+            onChange={onInitialInvestment}
+          ></input>
         </p>
         <p>
           <label>ANNUAL INVESTMENT</label>
-          <input type="number" required></input>
+          <input
+            type="number"
+            required
+            value={annualInvestmentValue}
+            onChange={onAnnualInvestment}
+          ></input>
         </p>
       </div>
       <div className="input-group">
         <p>
           <label>EXPECTED RETURN</label>
-          <input type="number" required></input>
+          <input
+            type="number"
+            required
+            value={expectedReturnValue}
+            onChange={onExpectedReturn}
+          ></input>
         </p>
         <p>
           <label>DURATION</label>
-          <input type="number" required></input>
+          <input
+            type="number"
+            required
+            value={durationValue}
+            onChange={onDuration}
+          ></input>
         </p>
       </div>
     </section>
